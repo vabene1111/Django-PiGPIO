99e25300644f273b251a0e5df257c072b3ff7bcc
vabene1111 <vabene1234@googlemail.com>
2019-04-10 12:35:18 +0200

    fixed program form

--- PiGPIO/forms.py
+++ PiGPIO/forms.py
@@ -5,9 +5,9 @@ from PiGPIO.models import Program
 
 
 class ProgramForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea)
-    code = forms.CharField(disabled=True, widget=forms.Textarea)
-    blockly_string = forms.CharField(disabled=True, widget=forms.Textarea)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    code = forms.CharField(disabled=True, widget=forms.Textarea, required=False)
+    blockly_string = forms.CharField(disabled=True, widget=forms.Textarea, required=False)
 
     class Meta:
         model = Program
