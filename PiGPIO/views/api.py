33f5cdcc96245272632ece3bb2ad0ec37b5ab1b7
vabene1111 <vabene1234@googlemail.com>
2019-04-10 13:54:12 +0200

    testing threading

--- PiGPIO/views/api.py
+++ PiGPIO/views/api.py
@@ -1,4 +1,5 @@
 import sys
+from threading import Thread
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.response import Response
@@ -20,27 +21,45 @@ class SetPinView(APIView, LoginRequiredMixin):
 
 class RunProgramView(APIView, LoginRequiredMixin):
     def post(self, request):
-        raspi.set_mode(0)
-        program_id = request.data['pk']
-        program = Program.objects.get(pk=program_id)
-
-        program.running = False
-
-        program.save()
-
-        try:
-            exec(program.code)
-            raspi.log(_('INFO: Program finished successfully!'))
-        except UndefinedPinException:
-            raspi.log('ERROR: Trying to set status of undefined pin')  # TODO localize
-        except OutputNotSupportedException as e:
-            raspi.log('ERROR: There was an error trying to output on ' + str(e))  # TODO localize
-        except ListNotSupportedException as e:
-            raspi.log('ERROR: Trying to pass list ' + str(e) + ' to function not supporting lists.')  # TODO localize
-        except Exception:
-            raspi.log(str(sys.exc_info()))
+        run_program(request.data['pk'])
 
         return Response({})
+
+
+def start_new_thread(function):
+    def decorator(*args, **kwargs):
+        t = Thread(target=function, args=args, kwargs=kwargs)
+        t.daemon = True
+        t.start()
+
+    return decorator
+
+
+@start_new_thread
+def run_program(program_id):
+    program = Program.objects.get(pk=program_id)
+    program.running = True
+    program.save()
+
+    Log.objects.all().delete()
+
+    raspi.set_mode(0)
+
+    try:
+        exec(program.code)
+        raspi.log(_('INFO: Program finished successfully!'))
+    except UndefinedPinException:
+        raspi.log('ERROR: Trying to set status of undefined pin')  # TODO localize
+    except OutputNotSupportedException as e:
+        raspi.log('ERROR: There was an error trying to output on pin' + str(e))  # TODO localize
+    except ListNotSupportedException as e:
+        raspi.log('ERROR: Trying to pass list ' + str(e) + ' to function not supporting lists.')  # TODO localize
+    except Exception:
+        raspi.log(str(sys.exc_info()))
+
+    program.running = False
+    program.save()
+    raspi.log('__DONE__')
 
 
 class StopProgramView(APIView, LoginRequiredMixin):
@@ -74,9 +93,13 @@ class PopLogView(APIView, LoginRequiredMixin):
     def post(self, request):
         logs = Log.objects.all()
 
+        done = False
         log_string = ""
         for log in logs:
-            log_string = log_string + log.created_at.strftime("%H:%M:%S:%f") + ' ' + log.data + '\n'
+            if log.data == "__DONE__":
+                done = True
+            else:
+                log_string = log_string + log.created_at.strftime("%H:%M:%S:%f") + ' ' + log.data + '\n'
             Log.objects.get(pk=log.pk).delete()
 
-        return Response({'log': log_string})
+        return Response({'log': log_string, 'program_finished': done})
